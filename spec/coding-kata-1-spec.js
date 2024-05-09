1a438a273df639afa430766a46898b458db3f751
vaaniicx <vanessakausl@gmail.com>
2024-05-09 16:19:01 +0200

    Formatting

--- spec/coding-kata-1-spec.js
+++ spec/coding-kata-1-spec.js
@@ -1,25 +1,25 @@
-describe('Find Spaceship', function() {
-	beforeEach(function() {
-		spaceship = new Spaceship();
-	});
-
-	it('Should return "Spaceship lost forever." for empty map', function() {
-		expect(spaceship.find(''))
-			.toEqual('Spaceship lost forever.');
-	});
-
-	it('Should return "Spaceship lost forever." for given map', function() {
-		expect(spaceship.find('..\n..'))
-			.toEqual('Spaceship lost forever.');
-	});
-
-    it('Should return [1,0] for given map', function() {
-        expect(spaceship.find('...\n.X.'))
-            .toEqual('Ship found at (1, 0).');
+describe('Find Spaceship', function () {
+    beforeEach(function () {
+        spaceship = new Spaceship();
     });
 
-	it('Should return [7,2] for given map', function() {
-        expect(spaceship.find('..........\n..........\n..........\n.......X..\n..........\n..........'))
-            .toEqual("Ship found at (7, 2).");
+    it('Should return "Spaceship lost forever." for empty map', function () {
+        expect(spaceship.find('')).toEqual('Spaceship lost forever.');
+    });
+
+    it('Should return "Spaceship lost forever." for given map', function () {
+        expect(spaceship.find('..\n..')).toEqual('Spaceship lost forever.');
+    });
+
+    it('Should return [1,0] for given map', function () {
+        expect(spaceship.find('...\n.X.')).toEqual('Ship found at (1, 0).');
+    });
+
+    it('Should return [7,2] for given map', function () {
+        expect(
+            spaceship.find(
+                '..........\n..........\n..........\n.......X..\n..........\n..........'
+            )
+        ).toEqual('Ship found at (7, 2).');
     });
 });
