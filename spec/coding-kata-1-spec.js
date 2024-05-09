efd2cf4edbd1eaa0ec6b47327a56cf55c76876b1
vaaniicx <vanessakausl@gmail.com>
2024-05-09 13:52:26 +0200

    Red: add test

--- spec/coding-kata-1-spec.js
+++ spec/coding-kata-1-spec.js
@@ -2,4 +2,9 @@ describe('Find Spaceship', function() {
 	beforeEach(function() {
 		spaceship = new Spaceship();
 	});
+
+	it('Should return "Spaceship lost forever." for given map', function() {
+		expect(spaceship.find(''))
+			.toEqual('Spaceship lost forever.');
+	})
 });
