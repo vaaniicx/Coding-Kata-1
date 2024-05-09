965e27f6f822aa240b6b3d3feaaa0db12254e5ba
vaaniicx <vanessakausl@gmail.com>
2024-05-09 16:18:54 +0200

    Refactor: to be more accurate

--- src/coding-kata-1.js
+++ src/coding-kata-1.js
@@ -1,23 +1,46 @@
+/**
+ * Self Study 1 – TDD Coding Kata “Help ALF“
+ *
+ * Late last night in the Tanner household, ALF was repairing his spaceship so he might get back to Melmac.
+ * Unfortunately for him, he forgot to put on the parking brake, and the spaceship took off during repair.
+ * Now it's hovering in space. ALF has the technology to bring the spaceship home if he can lock on to its location.
+ *
+ * The map will be given in the form of a string with \n separating new lines.
+ * The bottom left of the map is [0, 0]. X is ALF's spaceship.
+ *
+ * In this example:
+ * findSpaceship(map) => [7, 2]
+ * If you cannot find the spaceship, the result should be "Spaceship lost forever."
+ */
+
 function Spaceship() {}
-Spaceship.prototype.find = function(map) {
-	if (isShipLost(map)) {
-		return 'Spaceship lost forever.';
-	} else {
-		let coordinates = findCoordinates(map);
-		return 'Ship found at (' + coordinates[0] + ', ' + coordinates[1] + ').';
-	}
+Spaceship.prototype.find = function (map) {
+    if (isShipLost(map)) {
+        // Ship cannot be found
+        return 'Spaceship lost forever.';
+    }
+
+    let coordinates = findCoordinates(map);
+    return 'Ship found at (' + coordinates[0] + ', ' + coordinates[1] + ').';
 };
 
-function isShipLost(map) {
-	return !(map && map.includes('X'));
+function findCoordinates(map) {
+    let rows = map.split('\n');
+    let shipPosition = rows.findIndex((row) => hasShip(row));
+    if (shipPosition === -1) {
+        // Ship cannot be found
+        return null;
+    }
+
+    let x = rows[shipPosition].indexOf('X'); // Set x to the position of 'X' in the row
+    let y = rows.length - shipPosition - 1; // Subtract current index - 1 from the number of total rows
+    return [x, y];
 }
 
-function findCoordinates(map) {
-	let rows = map.split('\n');
+function isShipLost(map) {
+    return !(map && hasShip(map));
+}
 
-	for (let index = 0; index < rows.length; index++) {
-		if (rows[index].indexOf('X') > -1) {
-			return [rows[index].indexOf('X'), (rows.length - index) - 1];
-		}
-	}
+function hasShip(input) {
+    return input.indexOf('X') > -1;
 }
