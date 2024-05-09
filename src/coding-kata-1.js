1b42a4eaf14d799e1fefda07b0e4ea8d728b531a
vaaniicx <vanessakausl@gmail.com>
2024-05-09 14:35:36 +0200

    Refactor: to be more accurate

--- src/coding-kata-1.js
+++ src/coding-kata-1.js
@@ -3,10 +3,14 @@ Spaceship.prototype.find = function(map) {
 	if (isShipLost(map)) {
 		return 'Spaceship lost forever.';
 	} else {
-		return 'Ship found at (1, 0).'
+		return findCoordinates(map);
 	}
 };
 
 function isShipLost(map) {
 	return !(map && map.includes('X'));
+}
+
+function findCoordinates(map) {
+	return 'Ship found at (1, 0).';
 }
